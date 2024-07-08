710cd838c401f41ab63b06900fceb69aafb78867
apple-phi <lkn849@gmail.com>
2024-07-08 16:53:45 +0100

    Updates

---
+++ _app/immutable/chunks/modern-package-managers.CX-pzh0l.js
@@ -0,0 +1,1 @@
+import{g as t,t as n}from"./disclose-version.Dcc07gWw.js";var r=n("<p>In progress…</p>");const p={title:"Pixies, foxes and keyboard mashing",excerpt:"What modern package management gets right and wrong",date:"2024-07-08",updated:"2024-07-08",categories:["Opinion"]};function d(a){var e=r();t(a,e)}export{d as default,p as metadata};
